4c5aebcef0225183385d381104bed3c480456034
Duy Thai <ndthai2010@gmail.com>
2022-05-19 15:09:47 +0700

    Graph Degree Revision

---
+++ Revision/src/graph/GraphDegree.java
@@ -0,0 +1,44 @@
+package src.graph;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@SuppressWarnings({ "unchecked", "deprecation" })
+public class GraphDegree {
+
+    private int[][] matrix;// ma trận kề
+    private String[] v;// danh sách các đỉnh
+    private int n;// số đỉnh
+
+    public void loadGraphFromFile(String fileName) throws FileNotFoundException {
+        Scanner sc = new Scanner(new FileReader(fileName));
+        n = Integer.parseInt(sc.nextLine());
+        v = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            v[i] = sc.nextLine();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    public int[] getDegree() {
+        int[] degree = new int[n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 1){
+                    degree[i]++;
+                }
+            }
+        }
+        return degree;
+    }
+
+}
